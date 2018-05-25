130c5fb52b7b6051a6a1a6c870609ec13a90f4c0
clear329 <clearg1929@gmail.com>
2018-05-25 05:59:15 +0200

    add file picker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { FileChooser } from '@ionic-native/file-chooser';
+import { File } from '@ionic-native/file';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -24,6 +26,8 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
+    File,    
+    FileChooser,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { FileChooser } from '@ionic-native/file-chooser';
+import { StatusBar } from '@ionic-native/status-bar';
+import { FilePath } from '@ionic-native/file-path';
+// import { File, IFile, Entry } from '@ionic-native/file';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +11,33 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+    public navCtrl: NavController,
+    private fileChooser: FileChooser,
+    private statusBar: StatusBar,
+    private filePath: FilePath,
+    // private file: File
+  ) {
 
+  }
+
+  initialsetting() {
+    this.statusBar.overlaysWebView(true);
+    this.statusBar.backgroundColorByHexString('#ffffff');
+  }
+
+  openFile() {
+    this.fileChooser.open()
+      .then((uri) => {
+        this.fileChooser.open().then((url) => {
+          this.filePath.resolveNativePath(url)
+            .then((result) => {
+              console.log(result);
+            })
+        })
+
+      })
+      .catch(e => console.log(JSON.stringify(e)));
   }
 
 }
